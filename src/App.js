3f867f3682ef99204814f0f76bc56437d6db7952
สหสวรรษ อึ้งวณิชย์พันธ์ <42871459+sahasawatE@users.noreply.github.com>
2021-12-20 01:12:23 +0700

    update 20/12/2021

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ import School from './components/School';
 import Parent from './components/Parent';
 import './App.css';
 import axios from 'axios';
-import {selectSubjectContext} from './components/selectSubjectContext';
+import { selectSubjectContext } from './components/selectSubjectContext';
+import {socketContext} from './socketContext';
+import socketIOClient from 'socket.io-client';
+require('dotenv').config()
 
 export default function App() {
-  const api = axios.create({
-    baseURL: 'http://localhost:3001/',
-  });
+  const ngrok = process.env.REACT_APP_NGROK_URL;
+  const api = axios.create({ baseURL: ngrok })
 
   const [user,setUser] = react.useState([]);
   const homeRef = react.useRef(null);
   const classRef = react.useRef(null);
   const schoolRef = react.useRef(null);
   const parentRef = react.useRef(null);
+  const [socket,setSocket] = react.useState(null);
   
   const [selectSubject, setSelectSubject] = react.useState(null);
 
@@ -38,62 +41,63 @@ export default function App() {
   
   react.useEffect(() => {
     if(localStorage.getItem('auth-token') !== null){
+      setSocket(socketIOClient(process.env.REACT_APP_NGROK_URL, {
+        query: `token=${localStorage.getItem('auth-token')}`
+      }))
       getdata()
-    }
-    else{
-      console.log('login')
     }
   },[])
 
-  //add route for submit work
 
   return(
     <div className='main'>
     <Router>
       <userContext.Provider value={{user,setUser}}>
       <selectSubjectContext.Provider value={{selectSubject,setSelectSubject}}>
-      <Appbar/>
-      <Switch>
-        <Route exact path="/login">
-          {localStorage.getItem('auth-token') ? 
-          <Redirect to='/home'/>
-        :
-        <Login/>
-        }
-          
-        </Route>
-        <Route exact path='/home'>
-          {localStorage.getItem('auth-token') ? 
-          <Home forwardedRef={homeRef}/>
+      <socketContext.Provider value={{socket}}>
+        <Appbar/>
+        <Switch>
+          <Route exact path="/login">
+            {localStorage.getItem('auth-token') ? 
+            <Redirect to='/home'/>
           :
-          <Redirect to='/login'/>
+          <Login/>
           }
-        </Route>
-        <Route exact path='/studentWork'>
-          {localStorage.getItem('auth-token') ? 
-          <Class forwardedRef={classRef}/>
-          :
-          <Redirect to='/login'/>
-          }
-        </Route>
-        <Route exact path='/school'>
-          {localStorage.getItem('auth-token') ? 
-          <School forwardedRef={schoolRef}/>
-          :
-          <Redirect to='/login'/>
-          }
-        </Route>
-        <Route exact path='/parent'>
-          {localStorage.getItem('auth-token') ? 
-          <Parent forwardedRef={parentRef}/>
-          :
-          <Redirect to='/login'/>
-          }
-        </Route>
-        <Route path='*'>
-        {localStorage.getItem('auth-token') ? <Redirect to='/home'/> : <Redirect to='/login'/>}
-        </Route>
-      </Switch>
+            
+          </Route>
+          <Route exact path='/home'>
+            <Home forwardedRef={homeRef}/>
+            {/* {localStorage.getItem('auth-token') ? 
+            :
+            <Redirect to='/login'/>
+            } */}
+          </Route>
+          <Route exact path='/studentWork'>
+            <Class forwardedRef={classRef}/>
+            {/* {localStorage.getItem('auth-token') ? 
+            :
+            <Redirect to='/login'/>
+            } */}
+          </Route>
+          <Route exact path='/school'>
+            <School forwardedRef={schoolRef}/>
+            {/* {localStorage.getItem('auth-token') ? 
+            :
+            <Redirect to='/login'/>
+            } */}
+          </Route>
+          <Route exact path='/parent'>
+            <Parent forwardedRef={parentRef}/>
+            {/* {localStorage.getItem('auth-token') ? 
+            :
+            <Redirect to='/login'/>
+            } */}
+          </Route>
+          <Route path='*'>
+          {localStorage.getItem('auth-token') ? <Redirect to='/home'/> : <Redirect to='/login'/>}
+          </Route>
+        </Switch>
+      </socketContext.Provider>
       </selectSubjectContext.Provider>
       </userContext.Provider>
     </Router>
